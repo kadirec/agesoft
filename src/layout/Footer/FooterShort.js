c2bb9d7bd9eeefad228f289608d651bff538104a
kadirec <kadir@ageajans.com>
2022-06-14 15:34:21 +0300

    agesoft

--- src/layout/Footer/FooterShort.js
+++ src/layout/Footer/FooterShort.js
@@ -12,7 +12,7 @@ const FooterShort = () => {
             <div className="col-md-7 col-lg-7">
               <div className="copyright-text">
                 <p className="mb-lg-0 mb-md-0">
-                  &copy; 2015-2022 AgeSoft 
+                  &copy; 2015-2022 AgeSoft
 
                 </p>
               </div>
