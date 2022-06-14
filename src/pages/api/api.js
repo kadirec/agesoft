23e5ae97a0b22142860cc70f96cd9a9ab62e5fe2
hsnlbnan <hsnlbnan@gmail.com>
2022-06-14 23:20:01 +0300

    change api routes ...

--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -1,30 +1,38 @@
-export default function (req, res) {
+export default async (req, res) => {
   require("dotenv").config();
+  const nodemailer = require("nodemailer");
 
-  let nodemailer = require("nodemailer");
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
-    auth: {
-      user: "kariyer.ozsoygroup@gmail.com",
-      pass: "123456aA!",
-    },
-    secure: true,
-  });
+  async function mail() {
+    console.log("enter async function");
+    const transporter = nodemailer.createTransport({
+      name: "smtp.gmail.com",
+      port: 587,
+      host: "smtp.google.com",
+      auth: {
+        user: "kariyer.ozsoygroup@gmail.com",
+        pass: "123456aA!",
+      },
+      secure: true,
+    });
+    let mail = await transporter.sendMail({
+      from: "demo email",
+      to: "your email",
+      subject: `Demo Talebi ${req.body.name}`,
+      text: req.body.message + " | Gönderen " + req.body.email,
+      html: `<div>${req.body.message}</div><p> ${req.body.email}</p>`,
+    });
+  }
 
-  const mailData = {
-    from: "demo email",
-    to: "your email",
-    subject: `Demo Talebi ${req.body.name}`,
-    text: req.body.message + " | Gönderen " + req.body.email,
-    html: `<div>${req.body.message}</div><p> ${req.body.email}</p>`,
-  };
-
-  transporter.sendMail(mailData, function (err, info) {
-    if (err) console.log(err);
-    else console.log(info);
-  });
-
-  console.log(req.body);
-  res.send("success");
-}
+  try {
+    console.log("sending mail");
+    await mail();
+    res.status(200);
+    console.log("mail should be sent");
+  } catch (error) {
+    console.log(error);
+    console.log("error sending mail");
+    res.status(404);
+  } finally {
+    res.end();
+  }
+};
