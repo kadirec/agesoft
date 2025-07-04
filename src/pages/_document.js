7146580d5146cba4f1af76bae287c8e753de184e
Hüsnü Lübnan <55919762+hsnlbnan@users.noreply.github.com>
2025-07-04 11:58:47 +0300

    Block all web crawlers and add noindex meta tag
    Updated robots.txt to disallow all user agents from crawling the site. Added a meta tag to _document.js to instruct search engines not to index, follow, archive, or snippet the site. These changes help prevent the site from appearing in search engine results.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="robots" content="noindex, nofollow, noarchive, nosnippet" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&family=Open+Sans:wght@400;600&display=swap"
             rel="stylesheet"
